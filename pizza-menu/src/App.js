17a63184756cedf34a4504f5aa716f1fc4b246b3
Olena Surilova <elen21surilova@gmail.com>
2026-01-28 22:52:02 +0100

    feat: generate list of pizzas

--- pizza-menu/src/App.js
+++ pizza-menu/src/App.js
@@ -1,4 +1,5 @@
 import './index.css';
+import pizzaData from './data';
 
 function App() {
   return (
@@ -17,20 +18,31 @@ function Header() {
 }
 
 function Menu() {
+  const pizza = pizzaData;
+  const numPizza = pizza.length;
+
   return (
     <div className='menu'>
       <h2>Our Menu</h2>
-      <Pizza />
+      {numPizza > 0 ? <ul className='pizzas'>
+        {pizzaData.map(item => {
+          return (<Pizza key={item.name} pizza={item} />)
+        })}
+      </ul> : <p>We're still working on our menu. Please come back later :)</p>}
     </div>
   )
 }
 
-function Pizza() {
+function Pizza({ pizza }) {
   return (
-    <div className='pizza'>
-      <img alt="Pizza Spinaci" src="/pizzas/spinaci.jpg" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese.</p>
+    <div className={`pizza  ${pizza.soldOut ? 'sold-out' : ''}`}>
+      <img alt={pizza.name} src={pizza.photoName} />
+      <div>
+        <h3>{pizza.name}</h3>
+        <p>{pizza.ingredients}</p>
+        <span>{pizza.soldOut ? "SOLD OUT" : pizza.price}</span>
+      </div>
+
     </div>
   )
 }
@@ -43,8 +55,20 @@ function Footer() {
   console.log(isOpen);
 
   return (
-    <footer className='footer'>{new Date().toLocaleTimeString()} We're currently open!</footer>
+    <footer className='footer'>
+      {isOpen ? <Order closeHour={closeHour} /> :
+        <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>
+      }
+
+    </footer>
   )
+}
+
+function Order({ closeHour }) {
+  return <div className="order">
+    <p>We're open until {closeHour}:00. Visit us or order online.</p>
+    <button className='btn'>Order</button>
+  </div>
 }
 
 export default App;
